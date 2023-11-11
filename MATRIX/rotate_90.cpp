f2db2c8a1fc15205cf2e4bd6934c41c284409c4b
DILLI_DONKA <110531001+DILLIDONKA@users.noreply.github.com>
2023-11-11 22:36:23 +0530

    Create rotate_90.cpp

---
+++ MATRIX/rotate_90.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print_matrix(int *arr[],int m,int n){
+     for(int i=0;i<m;i++){
+          for(int j=0;j<n;j++){
+               cout<<arr[i][j]<<" ";
+          }
+          cout<<endl;
+     }
+}
+void transpose(int *arr[],int m,int n){
+     for(int i=0;i<m;i++){
+          for(int j=i+1;j<n;j++){
+               swap(arr[i][j],arr[j][i]);
+          }
+     }
+}
+void rotate_90(int *arr[],int m,int n){
+     transpose(arr,m,n);
+     for(int i=m-1;i>=0;i--){
+          for(int j=0;j<n;j++){
+               cout<<arr[i][j]<<" ";
+          }
+          cout<<endl;
+     }
+}
+
+int main(){
+     int m,n;
+     cout<<"Enter the no.of rows :";
+     cin>>m;
+     cout<<"Enter the no.of columns :";
+     cin>>n;
+     int *arr[m];
+     for(int i=0;i<m;i++){
+          arr[i]=new int[n];
+          for(int j=0;j<n;j++){
+               arr[i][j]=(i*i+j*j)/(i+1);
+
+          }
+     }
+     cout<<"original matrix is :"<<endl;
+     print_matrix(arr,m,n);
+     cout<<"matrix after rotate by 90 degrees is :"<<endl;
+     rotate_90(arr,m,n);
+     return 0;
+     
+}
